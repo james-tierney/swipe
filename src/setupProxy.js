5f38c830fc2bc00303173c12a9e084c2d03758d0
james-tierney <jamesytier@hotmail.com>
2024-09-29 13:08:07 +0100

    PROXY

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,8 +5,8 @@ module.exports = function (app) {
   app.use(
     "/create-checkout-session",
     createProxyMiddleware({
-      target: "https://swipe-v9h6.onrender.com",
-      //target: 'http://localhost:3002',
+      //target: "https://swipe-v9h6.onrender.com",
+      target: "http://localhost:3002",
       changeOrigin: true,
     })
   );
