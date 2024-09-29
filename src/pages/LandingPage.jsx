2e41a9680d43cd2feede89adcb0ea0a41bb3897a
james-tierney <jamesytier@hotmail.com>
2024-09-29 12:28:10 +0100

    client side debugging

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -17,7 +17,8 @@ const LandingPage = () => {
     let response;
     try {
       console.log("The auth token: ", authToken)
-      response = await fetch('https://swipe-v9h6.onrender.com/create-checkout-session', {
+      //response = await fetch('https://swipe-v9h6.onrender.com/create-checkout-session', {
+        response = await fetch('/create-checkout-session', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
